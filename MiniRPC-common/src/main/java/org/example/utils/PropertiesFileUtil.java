8a35f0ae153c2c9df1912180327840e9151b7c7b
xiaojie <1624565996@qq.com>
2025-05-27 21:00:07 +0800

    2025/5/27 2

---
+++ MiniRPC-common/src/main/java/org/example/utils/PropertiesFileUtil.java
@@ -0,0 +1,32 @@
+package org.example.utils;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Properties;
+
+@Slf4j
+public final class PropertiesFileUtil {
+    private PropertiesFileUtil() {}
+    public static Properties readPropertiesFile(String fileName) {
+        //获取类路径下某个资源文件的 URL，传入空字符串表示获取 类路径的根目录。
+        URL url = PropertiesFileUtil.class.getClassLoader().getResource("");
+        String rpcConfigPath = "";
+        if (url != null) {
+            rpcConfigPath = url.getPath()+fileName;
+        }
+        Properties properties = null;
+        try (InputStreamReader inputStreamReader = new InputStreamReader(
+                new FileInputStream(rpcConfigPath), StandardCharsets.UTF_8)) {
+            properties = new Properties();
+            properties.load(inputStreamReader);
+        } catch (IOException e) {
+            log.error("occur exception when read properties file [{}]", fileName);
+        }
+        return properties;
+    }
+}
--- MiniRPC-common/src/main/java/org/example/utils/StringUtil.java
+++ MiniRPC-common/src/main/java/org/example/utils/StringUtil.java
@@ -1,4 +1,15 @@
 package org.example.utils;
 
 public class StringUtil {
+    public static boolean isBlank(String str) {
+        if(str == null || str.length() == 0){
+            return true;
+        }
+        for(int i = 0; i < str.length(); i++){
+            if(!Character.isWhitespace(str.charAt(i))){
+                return false;
+            }
+        }
+        return true;
+    }
 }
